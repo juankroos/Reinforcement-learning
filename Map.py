55be12e5b9cf7066b7bc473ec633ed2bc74fe6d9
juankroos <58156316+juankroos@users.noreply.github.com>
2025-05-15 15:13:35 +0100

    push new arch

--- Map.py
+++ Map.py
@@ -5,7 +5,7 @@ class Map:
         self.name = name
         self.random_array = []
         self.rewards = []
-        self.level = level
+        self.level = [ math.sqrt((self.size[0]**2 - self.size[1]**2)/2) - 1 , math.sqrt((self.size[0]**2 - self.size[1]**2)/2) , math.sqrt((self.size[0]**2 - self.size[1]**2)/2) +1]
     
     # Intialze when the map is full of 0 cells
     def add_obstacle(self, obstacle):
@@ -20,6 +20,7 @@ class Map:
                     # Level 2   c >=  math.sqrt((self.size[0]**2 - self.size[1]**2)/2) 
                     # Level 3   c >=  math.sqrt((self.size[0]**2 - self.size[1]**2)/2) +1
 
+
                     if c >=  math.sqrt((self.size[0]**2 - self.size[1]**2)/2):
                         break
                 else:
@@ -31,10 +32,12 @@ class Map:
     # level 1: obstacle  randomized of 0 to the diagonal -1
     # level 2: obstacle randomized of 0 to the diagonal 
     # level 3: obstacle randomized of 0 to the diagonal +1
+
     def generate_obstacle(n,m):
         for i in range(n):
             for j in range(m):
                 if self.level == 1:
+                    #add_obstacle(0)
                     pass 
                 elif self.level == 2:
                     pass
@@ -44,7 +47,7 @@ class Map:
     def generate_table(self, n,m):
         """Generate a table of size n x m with random values."""
         self.random_array = np.random.rand(n, m)
-
+    # Initialize after the initialization of the map
     def generate_rewards(self,n,m):
         """Generate a table of size n x m with random rewards. for free cell"""
         for i in range(n):
@@ -52,4 +55,8 @@ class Map:
                 if self. random_array[n,m] == 0:
                     self.rewards = np.random.rand(1,3)
 
-    
+
+    # The next stpe is to generate random value and the table will be filled by rewards and obstacles 
+    # The next step is to make the agent capable of identify obstacle and reward and learn from it to chose the best direction
+    # And save those data !!important so if the map is the same the agent will not learn again and just go straight to the goal 
+    #
